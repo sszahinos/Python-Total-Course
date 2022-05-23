e8a6090aed23dbbd9fb065ce8b146b289a5db8af
sszahinos <sergiosaza6@gmail.com>
2022-05-23 11:28:38 +0200

    Day 1 project and configuration files

---
+++ Name Generator - Day 1/NameGenerator.py
@@ -0,0 +1,5 @@
+# Day 1 Project
+print("Your brand name will be: "
+      + input("What's your favorite colour?").capitalize()
+      + " "
+      + input("What's your favorite thing?").capitalize())
