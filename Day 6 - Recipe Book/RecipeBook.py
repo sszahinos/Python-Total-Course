fd2399b1f3bd7ec581edd63d1922bd3b4774b18e
sszahinos <sergiosaza6@gmail.com>
2022-05-25 23:06:17 +0200

    Day 6 structure created

--- Day 6 - Recipe Book/RecipeBook.py
+++ Day 6 - Recipe Book/RecipeBook.py
@@ -1,7 +1,57 @@
+from os import system
+
+def clear():
+    system("cls")
+
+def show_menu():
+    print("""--- MAIN MENU ---
+    1. Show recipe
+    2. Create recipe
+    3. Create category
+    4. Delete recipe
+    5. Delete category
+    6. Close Recipes Book""")
+
+def show_categories():
+    pass
+
+def show_recipes(category):
+    pass
+
+def check_category(category):
+    its_ok = False
+    if category.isnumeric() and 0 < category < 7:
+        its_ok = True
+    return its_ok
+
+def check_recipe(recipe):
+    pass
+
+def ask_category():
+    option = -1
+    while not check_category(option):
+        print("Choose a category:")
+        show_categories()
+        option = input()
+
+    return option
+
+def ask_recipe():
+    return input("Choose a recipe: ")
+
+def delete_category(category):
+    pass
+
+def delete_recipe(recipe):
+    pass
+
+def create_default_directories():
+    pass
+
 # Create directories if not exists
 
 # Greetings
-
+print("Welcome to your Recipes Book")
 # Tell where the folders are located and how many recipes has inside.
 
 # Show menu
