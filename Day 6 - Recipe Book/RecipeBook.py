cda9eb8ffef77528992336f1ad5888ee62ed071d
sszahinos <sergiosaza6@gmail.com>
2022-05-25 21:36:48 +0200

    Day 6 Started recipe program

---
+++ Day 6 - Recipe Book/RecipeBook.py
@@ -0,0 +1,32 @@
+# Create directories if not exists
+
+# Greetings
+
+# Tell where the folders are located and how many recipes has inside.
+
+# Show menu
+# 1 - Read recipe
+# Ask category
+# Ask recipe
+# Show recipe
+
+# 2 - Create recipe
+# Ask category
+# Ask recipe name
+# Ask content
+# Create content
+
+# 3 - Create category
+# Ask category
+# Create category
+
+# 4 - Delete recipe
+# Ask category
+# Ask recipe
+# Delete recipe
+
+# 5 - Delete category
+# Ask category
+# Delete category
+
+# 6 - End program
