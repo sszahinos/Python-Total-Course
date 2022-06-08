d431212677a0f99c54ae017cc34f3d7e2bb4e0ee
sszahinos <sergiosaza6@gmail.com>
2022-06-08 16:50:32 +0200

    Day 9 I forgot to add the total time and the current date. Solved

--- Day 9 - Serial Number Finder/SerialNumberFinder.py
+++ Day 9 - Serial Number Finder/SerialNumberFinder.py
@@ -1,15 +1,23 @@
-import os
-import re
+import os, re, math
 from pathlib import Path
+from datetime import date
+import time
+
 
 MAIN_PATH = Path(".\\Mi_Gran_Directorio")
 PATTERN = r"N\D{3}-\d{5}"  # example: Nryu-12365
 
 
 def start():
+    start_time = time.time()
     serial_nums = find_serial_nums(MAIN_PATH, PATTERN)
     print_serial_nums(serial_nums)
+    print_total_time(start_time)
 
+
+def print_total_time(start_time: float):
+    end_time = time.time()
+    print(f"Search duration: {math.ceil(end_time - start_time)}")
 
 def find_pattern(file_path, pattern: str) -> re.Match:
     opened_file = open(file_path, "r")
@@ -30,9 +38,10 @@ def find_serial_nums(path: Path, pattern: str) -> dict:
 
 
 def print_serial_nums(serials: dict):
+    print(f"Date: {date.today().day}/{date.today().month}/{date.today().year}")
     print("""FILE\tSERIAL NUMBER\n-------\t-------""")
     for item in serials:
         print(f"{item}\t{serials[item]}")
-
+    print(f"\nSerial numbers found: {len(serials)}")
 
 start()
