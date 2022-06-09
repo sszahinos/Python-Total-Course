d81a705b6122767f3a324ec0cbfe487d884b5f99
sszahinos <sergiosaza6@gmail.com>
2022-06-09 19:22:51 +0200

    Day 13 Tells the day and the time

--- Day 13 - Virtual Voice Assistant/virtual_assistant.py
+++ Day 13 - Virtual Voice Assistant/virtual_assistant.py
@@ -1,4 +1,5 @@
 import pyttsx3 as ptsx
+
 """import pywhatkit as pwk
 import yfinance as yf
 import pyjokes as pj
@@ -6,14 +7,19 @@ import webbrowser
 import datetime
 import wikipedia"""
 import speech_recognition as sr
+from datetime import date, datetime
 
 LANG = 'es-ES'
-#LANG = 'ca'
+# LANG = 'ca'
 
 id_es_es_1 = "id=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0"
 id_en_us_1 = "id=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 id_en_us_2 = "id=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0"
 
+#calendar = {0: "Monday", 1: "Tuesday", 2: "Wednesday",
+#            3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+calendar = {0: "Lunes", 1: "Martes", 2: "Miércoles",
+            3: "Jueves", 4: "Viernes", 5: "Sabado", 6: "Domingo"}
 
 # audio -> text
 def transform_audio2text():
@@ -49,6 +55,7 @@ def transform_audio2text():
             print("Unexpected error")
             return "I keep waiting"
 
+
 def transform_text2audio(message):
     engine = ptsx.init()
     engine.setProperty('voice', id_es_es_1)
@@ -57,10 +64,24 @@ def transform_text2audio(message):
     engine.say(message)
     engine.runAndWait()
 
-transform_text2audio(transform_audio2text())
-"""
-engine = ptsx.init()
-for voice in engine.getProperty('voices'):
-    print(voice)
-"""
 
+def display_voices():
+    engine = ptsx.init()
+    for voice in engine.getProperty('voices'):
+        print(voice)
+
+
+def tell_day():
+    today = date.today()
+    day = calendar.get(today.weekday())
+    transform_text2audio(f"Hoy es {day}")
+
+def tell_hour():
+    hour = datetime.now().hour
+    minutes = datetime.now().minute
+    transform_text2audio(f"Son las {hour} y {minutes}")
+
+# transform_text2audio(transform_audio2text())
+# display_voices()
+tell_day()
+tell_hour()
